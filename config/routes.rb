8d8007c6190fee2b790c158b605744c24e69beed
Sarah <sarahlchavez@gmail.com>
2021-07-01 05:42:24 -0700

    commit tree ab6132bde1a1203f52496d29fcb2e177b75da134

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :new, :create]
+  
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  patch 'articles/:id', to: 'articles#update'
+  
+  # can also simply add edit and update actions to the resources call in the routes file
+  # resources :articles, only: [:index, :show, :new, :create, :edit, :update] 
 end
